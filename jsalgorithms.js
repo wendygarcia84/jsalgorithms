8258ea09d7ea14f0467909c27ec730b66efb809f
wendygarcia84 <wendyadriana.gt@gmail.com>
2020-12-26 14:48:03 -0800

    algorithm where do I belong, sorts an array in ascending order and returns the index of where to insert a new number

--- jsalgorithms.js
+++ jsalgorithms.js
@@ -125,3 +125,17 @@ function bouncer(arr) {
 }
 
 console.log(bouncer([7, "ate", "", false, 9]));
+
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+// For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+// Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+function getIndexToIns(arr, num) {
+  arr.sort(function(a, b){return a - b});
+  console.log(arr);
+  let index = arr.findIndex(el => {
+    return el >= num;
+  });
+  return index >= 0 ? index : arr.length;
+}
+
+console.log(getIndexToIns([5, 3, 20, 3], 5));
