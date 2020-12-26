7c4bf84af1472bc0e584ad905f228a723cb098f6
wendygarcia84 <wendyadriana.gt@gmail.com>
2020-12-26 14:10:06 -0800

    slice and splice js algorithm

--- jsalgorithms.js
+++ jsalgorithms.js
@@ -79,7 +79,8 @@ function findElement(arr, func) {
   
   console.log(booWho(false));
 
-  // Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+// Return the provided string with the first letter of each word capitalized. 
+// Make sure the rest of the word is in lower case.
 // For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
 
 function titleCase(str) {
@@ -98,3 +99,16 @@ function titleCase(str) {
 }
 
 console.log(titleCase("I'm a little tea pot"));
+
+// You are given two arrays and an index.
+// Copy each element of the first array into the second array, in order.
+// Begin inserting elements at index n of the second array
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+  let newArr = arr2.slice(0, arr2.legth);
+  newArr.splice(n, 0, ...arr1);
+  return newArr;
+}
+
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
