5fb2d1da2ed5f65c255af9a073a602bc122ab0c6
wendygarcia84 <wendyadriana.gt@gmail.com>
2020-12-26 09:41:42 -0800

    adding truncate string algorithm, all test passed except one

--- jsalgorithms.js
+++ jsalgorithms.js
@@ -53,3 +53,14 @@ function repeatStringNumTimes(str, num) {
   }
   
   console.log(repeatStringNumTimes("abc", 3));
+
+//   Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+// Return the truncated string with a ... ending.
+
+function truncateString(str, num) {
+    let newString = str.slice(0, num);
+    if (newString.length >= num) newString += "...";
+    return newString;
+  }
+  
+  console.log(truncateString("A-tisket a-tasket A green and yellow basket", 888));
