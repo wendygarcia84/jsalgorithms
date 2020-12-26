dae905dbbcda655510aa75725090bb246602dce3
wendygarcia84 <wendyadriana.gt@gmail.com>
2020-12-26 09:37:52 -0800

    adding largest numbers in arrays algorithm, working

--- jsalgorithms.js
+++ jsalgorithms.js
@@ -11,3 +11,24 @@ function findLongestWordLength(str) {
     return max;
   }
   console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+// Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+  function largestOfFour(arr) {
+    let largest = [];
+    arr.forEach(el => {
+      largest.push(Math.max(...el));
+    });
+    return largest;
+  }
+  
+  console.log(largestOfFour(
+    [[4, 5, 1, 3], 
+     [13, 27, 18, 26], 
+     [32, 35, 37, 39], 
+     [1000, 1001, 857, 1]
+    ]
+  ));
+
+  
