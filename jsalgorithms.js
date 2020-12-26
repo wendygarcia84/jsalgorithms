2c88b5f8e50e27d305f721f4ca919d0e180e87e6
wendygarcia84 <wendyadriana.gt@gmail.com>
2020-12-26 10:12:12 -0800

    finders keepers algorithm working

--- jsalgorithms.js
+++ jsalgorithms.js
@@ -63,3 +63,9 @@ function truncateString(str, num) {
   }
   
   console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8999));
+
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+function findElement(arr, func) {
+    return arr.find(func);
+  }
+  console.log(findElement([1, 5, 3, 8], num => num % 2 === 0));
